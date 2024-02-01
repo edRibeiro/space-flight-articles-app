b36ecfca94e6f37b40538a0e4efc53b4a06bb197
edRibeiro <edersonribeiro.ita@gmail.com>
2024-01-31 23:13:06 -0300

    Adicionando rotas GET.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ArticleController::class)->group(function () {
+    Route::get('/articles/{id}', 'show')->name('api.articles.show');
+    Route::get('/articles', 'index')->name('api.articles.index');
 });
