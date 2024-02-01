3d33a921175bf0210ef4afa48a82d63b99eefd4c
edRibeiro <edersonribeiro.ita@gmail.com>
2024-01-31 23:11:26 -0300

    Configurando Seeders.

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Database\Factories\ArticleFactory;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class ArticleSeeder extends Seeder
@@ -12,6 +15,21 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        ArticleFactory::factory()
+            ->count(10)
+            ->state(new Sequence(
+                fn (Sequence $sequence) => [
+                    'article_id' => $sequence->index,
+                    'title' => fake()->sentence(),
+                    'url' => fake()->url(),
+                    'image_url' => fake()->imageUrl(),
+                    'news_site' => fake()->sentence(2),
+                    'summary' => fake()->sentence(8),
+                    'published_at' => fake()->dateTime(),
+                    'last_updated_at' => fake()->dateTime(),
+                    'featured' => fake()->boolean()
+                ],
+            ))
+            ->create();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Article;
+use Database\Factories\ArticleFactory;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Article::factory()
+            ->count(10)
+            ->state(new Sequence(
+                fn (Sequence $sequence) => [
+                    'article_id' => $sequence->index,
+                    'title' => fake()->sentence(),
+                    'url' => fake()->url(),
+                    'image_url' => fake()->imageUrl(),
+                    'news_site' => fake()->sentence(2),
+                    'summary' => fake()->sentence(8),
+                    'published_at' => fake()->dateTime(),
+                    'last_updated_at' => fake()->dateTime(),
+                    'featured' => fake()->boolean()
+                ],
+            ))
+            ->create();
     }
 }
