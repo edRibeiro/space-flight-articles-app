63f47b21bff2826c75725660ed3d2ffed8c88451
edRibeiro <edersonribeiro.ita@gmail.com>
2024-02-02 22:11:02 -0300

    feat!: enable dependency injection in services and repositories.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Repositories\ArticleRepository;
+use App\Repositories\Contracts\ArticleRepositoryInterface;
+use App\Repositories\Contracts\SpaceFlightArticleRepositoryInterface;
+
+use App\Repository\SpaceFlightArticleRepository;
+use App\Services\ArticleService;
+use App\Services\Contracts\ArticleServiceInterface;
+use App\Services\Contracts\SpaceFlightArticleServiceInterface;
+use App\Services\SpaceFlightArticleService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ArticleRepositoryInterface::class, ArticleRepository::class);
+        $this->app->bind(SpaceFlightArticleRepositoryInterface::class, SpaceFlightArticleRepository::class);
+        $this->app->bind(ArticleServiceInterface::class, ArticleService::class);
+        $this->app->bind(SpaceFlightArticleServiceInterface::class, SpaceFlightArticleService::class);
     }
 
     /**
